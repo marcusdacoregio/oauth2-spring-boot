6408e1e52d7fe6a3043646f576c74931ae85ef06
Marcus Hert Da Corégio <marcusdacoregio@gmail.com>
2019-04-09 20:17:04 -0300

    Create the config-server with default setup

---
+++ config-server/src/main/java/com/marcus/configserver/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.marcus.configserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+}
