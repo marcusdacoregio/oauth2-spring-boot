4910f09af3f9d0f756c89bc826930b1189c040ab
Marcus Hert Da Corégio <marcusdacoregio@gmail.com>
2019-04-11 09:09:14 -0300

    Create gateway service and added its configuration on config service

---
+++ gateway-service/src/main/java/com/marcusdacoregio/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,17 @@
+package com.marcusdacoregio.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+}
