64aae526f27480550d9576701bf50026b1ae23b2
Marcus Hert da Corégio <marcusdacoregio@gmail.com>
2019-04-15 01:22:21 -0300

    Create user repository and custom user details service

---
+++ auth-service/src/main/java/com/marcusdacoregio/authservice/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.marcusdacoregio.authservice.repository;
+
+import com.marcusdacoregio.authservice.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+
+}
--- auth-service/src/main/java/com/marcusdacoregio/authservice/service/CustomUserDetailsService.java
+++ auth-service/src/main/java/com/marcusdacoregio/authservice/service/CustomUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.marcusdacoregio.authservice.service;
 
+import com.marcusdacoregio.authservice.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,9 +9,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username " + username + " not found"));
     }
 
 }
