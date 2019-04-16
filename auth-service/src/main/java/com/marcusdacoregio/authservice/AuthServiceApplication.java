de5f3328dfee77a232a901c082294f1194ea50c3
Marcus Hert da Corégio <marcusdacoregio@gmail.com>
2019-04-16 17:23:19 -0300

    Remove EnableResourceServer and add docker-compose.yml

--- auth-service/src/main/java/com/marcusdacoregio/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/marcusdacoregio/authservice/AuthServiceApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-@EnableResourceServer
 @EnableDiscoveryClient
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthServiceApplication {
