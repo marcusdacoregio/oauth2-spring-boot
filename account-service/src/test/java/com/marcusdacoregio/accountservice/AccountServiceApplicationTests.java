12777a8cc485723baec42390ec795097153bd83a
Marcus Hert da Corégio <marcusdacoregio@gmail.com>
2019-04-24 11:18:13 -0300

    Create account service project and its configuration

---
+++ account-service/src/test/java/com/marcusdacoregio/accountservice/AccountServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marcusdacoregio.accountservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccountServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
