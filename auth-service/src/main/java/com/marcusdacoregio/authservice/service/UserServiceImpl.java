0fd55507e0abffd8e04cae979113bacb4cb2dcdc
Marcus Hert da Corégio <marcusdacoregio@gmail.com>
2019-05-06 20:28:03 -0300

    Fix method signature

--- auth-service/src/main/java/com/marcusdacoregio/authservice/service/UserServiceImpl.java
+++ auth-service/src/main/java/com/marcusdacoregio/authservice/service/UserServiceImpl.java
@@ -23,19 +23,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(User user) {
-        throwIfUsernameOrEmailExists(user.getUsername());
+        throwIfUsernameExists(user.getUsername());
 
         String hash = passwordEncoder.encode(user.getPassword());
         user.setPassword(hash);
         user.setActivated(Boolean.TRUE); // TODO send sms or email with code for activation
         user.setAuthorities(new HashSet<>(Collections.singletonList(Authorities.ROLE_USER)));
-
+u
         // TODO other routines on account creation
 
         return userRepository.save(user);
     }
 
-    private void throwIfUsernameOrEmailExists(String username) {
+    private void throwIfUsernameExists(String username) {
         Optional<User> existingUser = userRepository.findByUsername(username);
         existingUser.ifPresent((user) -> {
             throw new IllegalArgumentException("User not available");
